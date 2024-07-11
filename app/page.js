a8fb349b588efedf645a943ea0fd01ac430755ea
Shoaib-777 <164485277+Shoaib-777@users.noreply.github.com>
2024-07-11 19:29:45 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
+    <>
     <div className="text-center bg-black text-white text-3xl m-auto">Home</div>
+    <div className="mx-auto flex justify-center items-center mt-[7rem]">
+      <Link href={'/login'}><button className="text-3xl text-bold text-blue-700 underline decoration-red-600">Click here to login or View Dashboard</button></Link>
+    </div>
+    </>
   );
 }
