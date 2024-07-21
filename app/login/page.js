8da1233dcd2da6b47a201388e1abdaa2a69d4d44
Shoaib-777 <164485277+Shoaib-777@users.noreply.github.com>
2024-07-21 20:09:51 +0530

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,30 +1,47 @@
 'use client'
 import { useFormState } from "react-dom";
 import { authenticate } from "../lib/actionu";
+import { useState } from "react";
+import {LuEye,LuEyeOff} from 'react-icons/lu'
 
 const LoginForm = () => {
     const [state, formAction] = useFormState(authenticate, undefined);
+    const [show,setShow]=useState(false)
 
-  return (
-    <div className='bg-blue-950 h-[95vh] pt-20'>
-            <div className=' mx-auto  border border-white w-[300px] h-[290px] pt-6 pl-4 pb-8'>
-                <div className='py-4 '>
-                    <h2 className='text-2xl text-white '>Login</h2>
-                </div>
-                <div className='pb-6'>
-                    <form action={formAction}>
-                        <div className='mb-4'>
-                            <input type="text" name='username' placeholder='User Name' className='px-3 py-2' />
-                        </div>
-                        <div>
-                            <input type="password" name='password' placeholder='Password' className='px-3 py-2' />
-                        </div>
-                        <button className="bg-green-500 black px-3 py-2 w-[245px] font-bold mt-3">Login</button>
-                        <span className="text-white font-bold  ml-9">{state && state}</span>
-                    </form>
-                </div>
+
+  return ( 
+   <>
+     <div className="background w-full h-full">
+            <div className="mx-auto border border-gray-200 w-[370px] h-[550px] sm:w-[400px] sm:h-[550px] px-6 mt-[1rem] rounded-lg bg-transparent mb-[3rem] ">
+            <form action={formAction}>
+            <div className=" mt-[2rem] ">
+                    <h2 className="text-center text-white font-bold text-2xl">Login</h2>
             </div>
-    </div>
+            <div>
+                    <h2 className="font-bold text-3xl text-center text-white mt-[3rem] mb-[2rem] ">Welcome Back !</h2>
+            </div>
+            <div className="mb-2">
+                    <label htmlFor="" className="text-white font-bold text-[18px]">User Name</label><br />
+                    <input type="text" name="username" placeholder="User Name" className="w-full px-4 py-2" />
+            </div>                
+            <div className="mb-2 relative">
+                    <label htmlFor=""className="text-white font-bold text-[18px]" >Password</label><br />
+                    <input name='password'  type={show ? 'text' : 'password'} placeholder="Password" className="w-full px-4 py-2 " />
+                    {
+                        show? 
+                        <LuEyeOff onClick={()=>setShow(!show)} className="absolute top-8 right-2 text-black w-7 h-7  cursor-pointer" />
+                        :
+                        <LuEye onClick={()=>setShow(!show)} className="absolute top-8 right-2 text-black w-7 h-7  cursor-pointer" />
+                    }
+            </div>
+            <div className="mt-[3rem]">
+                    <button className="w-full hover:bg-gray-950 text-center py-2 border border-white rounded-md bg-black text-white font-bold  ">Login</button>
+            </div>
+                <span className="text-white font-bold  ml-9">{state && state}</span>
+                </form>        
+            </div>
+        </div>
+    </>
   )
 }
 
